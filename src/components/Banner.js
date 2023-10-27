61293f655994181e5ffcfca03e18c4f1d16a83be
yunwoo jung <browniris7@naver.com>
2023-10-27 15:54:24 +0900

    메인 배너 만들기

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = ({movie}) => { // Home의 movie props 가져옴
+    console.log("movie?", movie);
+  return (
+    <div className='banner'
+    style={{backgroundImage: "url("+
+        `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.poster_path}`+
+    ")",
+    }}
+>
+    <div className='banner-info'>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { movieAction } from '../redux/actions/movieAction'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Banner from '../components/Banner'
 
 const Home = () => {
     const dispatch = useDispatch();
-    useEffect(() => {
+    // movieReducer 통해 영화 리스트 정보 가져오기
+    const {popularMovies, topRatedMovies, upComingMovies} = useSelector(state=>state.movie)
+    console.log("home", popularMovies); // 인기순 영화 정보 출력
+    
+
+    useEffect(() => { // 렌더링 이뤄진 후 호출
         dispatch(movieAction.getMovies());
     }, []);
 
   return (
-    <div>Home</div>
+    <div>
+      {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+    </div>
   )
 }
 
