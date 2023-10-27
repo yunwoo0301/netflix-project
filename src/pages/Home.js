3684fee42abb23ce84879e318f623d328488ca44
yunwoo jung <browniris7@naver.com>
2023-10-27 16:15:46 +0900

    슬라이드 메뉴 - 리액트 멀티 캐러셀 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { movieAction } from '../redux/actions/movieAction'
 import { useDispatch, useSelector } from 'react-redux'
 import Banner from '../components/Banner'
+import MovieSlide from '../components/MovieSlide'
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -18,6 +19,13 @@ const Home = () => {
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+    
+      <h1>Popular Movie</h1>
+      <MovieSlide movies={popularMovies}/>
+      <h1>Top rated Movie</h1>
+      <MovieSlide movies={topRatedMovies}/>
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies}/>
     </div>
   )
 }
