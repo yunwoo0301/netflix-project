9c097abc7aa3c658a2754fc25f08a9813e5fcc00
yunwoo jung <browniris7@naver.com>
2023-10-27 17:00:05 +0900

    로딩 스피너 - 조건부 렌더링 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,12 @@ import { movieAction } from '../redux/actions/movieAction'
 import { useDispatch, useSelector } from 'react-redux'
 import Banner from '../components/Banner'
 import MovieSlide from '../components/MovieSlide'
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
     const dispatch = useDispatch();
     // movieReducer 통해 영화 리스트 정보 가져오기
-    const {popularMovies, topRatedMovies, upComingMovies} = useSelector(state=>state.movie)
+    const {popularMovies, topRatedMovies, upComingMovies, loading} = useSelector(state=>state.movie)
     console.log("home", popularMovies); // 인기순 영화 정보 출력
     
 
@@ -16,18 +17,28 @@ const Home = () => {
         dispatch(movieAction.getMovies());
     }, []);
 
-  return (
-    <div>
-      {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
-    
-      <h1>Popular Movie</h1>
-      <MovieSlide movies={popularMovies}/>
-      <h1>Top rated Movie</h1>
-      <MovieSlide movies={topRatedMovies}/>
-      <h1>Upcoming Movie</h1>
-      <MovieSlide movies={upComingMovies}/>
-    </div>
-  )
+    // loading이 true면 로딩 스피너 노출 / false일 때 데이터를 보여줌
+    if (loading) { 
+      return <ClipLoader color="#ffff" loading={loading} size={150} />;
+    }
+  
+    return (
+      <div>
+        <ClipLoader
+          color="#ffff"
+          loading={loading}
+          size={150}
+        />
+        {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      
+        <h1>Popular Movie</h1>
+        <MovieSlide movies={popularMovies}/>
+        <h1>Top rated Movie</h1>
+        <MovieSlide movies={topRatedMovies}/>
+        <h1>Upcoming Movie</h1>
+        <MovieSlide movies={upComingMovies}/>
+      </div>
+    )
 }
 
 export default Home;
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -4,48 +4,39 @@ const API_KEY = process.env.React_APP_API_KEY;
 
 function getMovies() { // movie 정보를 가져올 함수 생성 
     return async (dispatch) => {  // 미들웨어  - 함수가 함수를 리턴함
-        // const popularMovieApi = await api.get(`/movie/popular?language=en-US&page=1`) // api.http함수
-        const popularMovieApi = api.get(
+        try { // error handling 위해 try - catch문 활용
+            dispatch({ type: "GET_MOVIES_REQUEST" });
+            const popularMovieApi = api.get( // api.http함수
             `/movie/popular?&language=en-US&page=1`
         );
 
-        // let url = `/movie/popular?language=en-US&page=1`;
-        // let response = await fetch(url);
-        // let data = await response.json();
+        const topRatedApi = api.get(
+            `/movie/top_rated?language=en-US&page=1`
+        );
 
-        // let url2 = `/movie/top_rated?language=en-US&page=1`;
-        // let response2 = await fetch(url2);
-
-        // let url3 = `/movie/upcoming?language=en-US&page=1`;
-        // let res = await fetch(url3);
-
-    const topRatedApi = api.get(
-        `/movie/top_rated?language=en-US&page=1`
-    );
-
-    const upComingApi = api.get(
-        `/movie/upcoming?language=en-US&page=1`
-    );
+        const upComingApi = api.get(
+            `/movie/upcoming?language=en-US&page=1`
+        );
 
 
-    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
-        popularMovieApi, 
-        topRatedApi, 
-        upComingApi,
-    ]);
-    dispatch({
-        type: "GET_MOVIES_SUCCESS",
-        payload: { 
-            popularMovies:popularMovies.data, 
-            topRatedMovies:topRatedMovies.data, 
-            upComingMovies:upComingMovies.data,
-        },
-    });
-
-    console.log(popularMovies);
-    console.log(topRatedMovies);
-    console.log(upComingMovies);
-
+        let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([
+            popularMovieApi, 
+            topRatedApi, 
+            upComingApi,
+        ]);
+            dispatch({
+                type: "GET_MOVIES_SUCCESS",
+                payload: { 
+                    popularMovies:popularMovies.data, 
+                    topRatedMovies:topRatedMovies.data, 
+                    upComingMovies:upComingMovies.data,
+                    loading: false,
+                },
+            });
+        
+        } catch (error) { // 에러 관리 구간
+            dispatch({ type:"GET_MOVIES_FAILURE" });
+        }       
     };
 }
 
