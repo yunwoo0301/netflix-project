3fd2c6ff4dd7a6c65ded84586dcb2fa6c2a4d29b
yunwoo jung <browniris7@naver.com>
2023-10-27 18:26:36 +0900

    타이틀 & 시청연령 & 장르 1차 추가

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Badge } from 'react-bootstrap';
 
 const MovieCard = ({item}) => { // item props로 받음
   return (
-    <div className='card' 
+    <div className='card_img' 
     style={{backgroundImage: "url("+
             `https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/${item.poster_path}`+
         ")",
     }}
     >
-        MovieCard
+        <div className='overlay'> 
+            <h1>{item.title}</h1>
+            <div>
+                {item.genre_ids.map((id)=>(
+                <Badge bg="danger">{id}</Badge>
+                ))}
+            </div>
+
+            <div>
+                <span>{item.vote_average}</span>
+                <span>{item.adult?"청불" : "Under 18"}</span>
+            </div>
+        </div>
     </div>
   )
 };
